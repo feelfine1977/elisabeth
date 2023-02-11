2765bf4179a2c08798be1c2d4743afa9dbd12476
feelfine1977 <feelfine1977@yahoo.de>
2023-02-11 09:41:11 +0100

    erste program Elisabeth

---
+++ start.py
@@ -0,0 +1,20 @@
+# write simple Hello World program
+
+print("Hallo Welt!")
+print("ich bin geboren")
+
+#name = input("Wie heißt du? ")
+#alter = input("Wie alt bist du? ")
+#print("Hallo " + name + ", du bist " + alter + " Jahre alt.")
+
+
+
+zahl1 = input('Gib eine Zahl ein: ')
+zahl2 = input('Gib eine zweite Zahl ein: ')
+
+summe = int(zahl1) + int(zahl2)
+multiplizieren = int(zahl1) * int(zahl2)
+
+print('Die Summe ist: ' + str(summe))
+print('Das Produkt ist: ' + str(multiplizieren))
+
